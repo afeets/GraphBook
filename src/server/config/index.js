44162bfac81634915cb8cba500b9885e8fd018d0
Andy Feetenby <AndyFeetenby@gmail.com>
2024-04-05 08:30:53 +0700

    Added sequelize to connect to mysql database. Created config file

---
+++ src/server/config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "development": {
+    "username": "graphuser",
+    "password": "Blueboy13!",
+    "database": "graphbook_dev",
+    "host": "localhost",
+    "dialect": "mysql",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
+  }, "production": {
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": process.env.database,
+    "host": process.env.host,
+    "logging": false,
+    "dialect": "mysql",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
+  }
+}
