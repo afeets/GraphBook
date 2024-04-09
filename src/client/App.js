db79d0c72e29f5a74baa95cac224c21f8a9283b6
Andy Feetenby <AndyFeetenby@gmail.com>
2024-04-09 14:22:56 +0700

    gql query with useState mutation of posts to cache response and update onSubmit

--- src/client/App.js
+++ src/client/App.js
@@ -1,79 +1,18 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
+import Feed from './Feed';
 import '../assets/css/style.css';
 import { gql } from '@apollo/client';
 
-const initialPosts = [
-  {
-      id: 2,
-      text: 'Lorem ipsum',
-      user: {
-          avatar: '/uploads/avatar1.png',
-          username: 'Test User'
-      }
-  },
-  {
-      id: 1,
-      text: 'Lorem ipsum',
-      user: {
-          avatar: '/uploads/avatar2.png',
-          username: 'Test User 2'
-      }
-  }
-];
-
-
-
 const App = () => {
   
-  const [ posts, setPosts ] = useState(initialPosts);
-  const [ postContent, setPostContent ] = useState('');
-  
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newPost = {
-      id: posts.length + 1,
-      text: postContent,
-      user: {
-        avatar: '/uploads/avatar1.png',
-        username: 'Fake User'
-      }
-    };
-
-    setPosts([newPost, ...posts ]);
-    console.log(posts);
-    setPostContent('');
-  }
-
   return (
     <div className="container">
       <Helmet>
         <title>Graphbook - Feed</title>
         <meta name="description" content="Newsfeed of all users on Graphbook" />
       </Helmet>
-      <div className="postForm">
-        <form onSubmit={handleSubmit}>
-          <textarea value={postContent} onChange={(e) =>
-            setPostContent(e.target.value)}
-            placeholder="Write your custom post" 
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      <div className="feed">
-        { posts.map((post, i) => 
-          <div key={post.id} className="post">
-            <div className="header">
-              <img src={post.user.avatar} />
-              <h2>{ post.user.username}</h2>
-            </div>
-            <p className="content">
-              { post.text }
-            </p>
-          </div>  
-        )}
-      </div>
+      <Feed />
     </div>
   )
 }
